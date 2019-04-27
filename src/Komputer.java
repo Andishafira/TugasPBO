c904ff1cae50871d606a941b89505970b22a14df
Andishafira <andishafira757@gmail.com>
2019-04-27 17:10:25 +0700

    kelas utama CPU dan komputer

--- src/Komputer.java
+++ src/Komputer.java
@@ -1,14 +1,17 @@
 public class Komputer {
 
         private CPU cpu = new CPU();
-
         Komputer(){}
 
         public void memasang(CPU cpu){
                 this.cpu = cpu;
-                System.out.println("CPU : "+this.cpu.getCPUData()+" GHz dicabut");
-
+                System.out.println("CPU : "+this.cpu.getCPUData()+" GHz dipasang");
         }
-public void mencabut(){}
+        public void mencabut(){
+                System.out.println("CPU : "+this.cpu.getCPUData()+" GHz dicabut");
+        }
+        public void cetakinfo(){
+                System.out.println("Spesifikasi :\n CPU : "+cpu.getCPUData()+"GHz");
+        }
 
 }
